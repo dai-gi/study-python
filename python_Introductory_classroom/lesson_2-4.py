2144a96fbe894c083a55f139ffa34d6f74e88d28
daisuke524 <daisuke.u524@gmail.com>
2020-01-26 12:03:19 +0900

    VScodeだと教材通りに表示されない実行結果が3箇所ある。 lesson_2-4.py 【計算を入力してみよう】 lesson_2-5.py 【計算をしてみよう】 【ウィンドウも表示できる】

--- python_Introductory_classroom/lesson_2-4.py
+++ python_Introductory_classroom/lesson_2-4.py
@@ -2,13 +2,12 @@
 # 【IDLEを起動する】
 # 【プロンプトに命令を入力する】
 
-# 【計算式を入力してみよう】
+# 【計算式を入力してみよう】未解決
 1+1 
-# [教材通りの実行結果が出ない]
-# 1. VScodeからpythonファイルを実行
-# 2. /Users/uedadaisuke/Downloads/anaconda3/bin/python /Users/uedadaisuke/github/study_python/python_Introductory_classroom/lesson_2-4.py
-# 3. 上記が表示される 
-# 4. ターミナルインタラクティブモードで直接「>>>1+1」と入力
+# 1. VScodeにて、ターミナルでpythonファイルを実行
+# 2. 「/Users/uedadaisuke/Downloads/anaconda3/bin/python /Users/uedadaisuke/github/study_python/python_Introductory_classroom/lesson_2-4.py」
+# 3. 上記の実行結果が表示される 
+# 4. ターミナルにて、インタラクティブモードで直接「>>>1+1」と入力
 # 5. 「>>>2」と表示される
 
 # 【本書での記述方法】
--- python_Introductory_classroom/lesson_2-5.py
+++ python_Introductory_classroom/lesson_2-5.py
@@ -1,10 +1,37 @@
+# 『 インタラクティブモードで遊んでみよう 』
 
- 
+# 【計算してみよう】未解決
+# 1. VScodeで計算式を入力し、ターミナルでPythonファイルを実行
+# 2. 「/Users/uedadaisuke/Downloads/anaconda3/bin/python /Users/uedadaisuke/github/study_python/python_Introductory_classroom/lesson_2-5.py」
+# 3. 上記のような実行結果が出る
+# 4. ターミナルにて、インタラクティブモードで直接入力
+# 5. 教材と同じ実行結果が表示される
+
+
+# 【Pythonは行頭の空白に意味がある】
+# 【文法エラーの時】
+
+
+# 【命令を実行してみよう】
 import calendar
 print(calendar.month(2020,1))
 
 
+# 【ウィンドウも表示できる】未解決
 import tkinter
-tkinter.Label(None, text="Hello").pack() 
+tkinter.Label(None, text="Hello").pack()
+# VScodeにて、ターミナルでpythonファイルを実行するが、実行結果が表示されない
+# ターミナルにて、インタラクティブモードで入力すると、実行結果がちゃんと表示される 
+import tkinter
+tkinter.Label(None, text="World").pack()
+# VScodeにて、ターミナルでpythonファイルを実行するが、実行結果が表示されない
+# ターミナルにて、インタラクティブモードで入力すると、実行結果がちゃんと表示される 
+
+
+# 【命令が分かれば何でもできる】
+
+
+
+
 
 
