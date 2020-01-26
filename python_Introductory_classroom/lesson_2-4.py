0dce9abf657ca8464a03f1aba822295cfd42d8d3
daisuke524 <daisuke.u524@gmail.com>
2020-01-26 11:05:36 +0900

    いちばんやさしいPython入門教室

---
+++ python_Introductory_classroom/lesson_2-4.py
@@ -0,0 +1,16 @@
+# 『 簡単な命令を実行してみよう 』
+# 【IDLEを起動する】
+# 【プロンプトに命令を入力する】
+
+# 【計算式を入力してみよう】
+1+1 
+# [教材通りの実行結果が出ない]
+# 1. VScodeからpythonファイルを実行
+# 2. /Users/uedadaisuke/Downloads/anaconda3/bin/python /Users/uedadaisuke/github/study_python/python_Introductory_classroom/lesson_2-4.py
+# 3. 上記が表示される 
+# 4. ターミナルインタラクティブモードで直接「>>>1+1」と入力
+# 5. 「>>>2」と表示される
+
+# 【本書での記述方法】
+
+
