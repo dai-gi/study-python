e5df6869c3534285baa102549bcbd7f40846c858
daisuke524 <daisuke.u524@gmail.com>
2020-01-31 00:57:37 +0900

    いちばんやさしいPython入門教室

---
+++ python_Introductory_classroom/Chapter_4/lesson_4-3.py
@@ -0,0 +1,36 @@
+# for構文
+# coding:utf-8
+for a in[1,2,3,4,5]:
+    print(a)
+
+for b in[1,100]:
+    print(b)
+
+for i in[1,2,3,4,5]:
+    print(i)
+    print("こんにちは")
+
+for i in[1,2,3,4,5]:
+    print(i)
+print("こんにちは")
+
+for i in range(1,5+1):
+    print(i)
+    print("こんにちは")
+
+for i in range(1,100+1):
+    print(i)
+    print("こんにちは")
+
+for i in range(1,1000+1):
+    print(i)
+
+for i in range(0,100):
+    print(i)
+
+for i in range(100):
+    print(i+1)
+    print("こんにちは")
+
+for i in "Hello":
+    print(i)
