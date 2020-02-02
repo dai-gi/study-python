acc1a8ad7d483ed5876a3fb9297d215ae68f52f6
daisuke524 <daisuke.u524@gmail.com>
2020-02-03 07:34:14 +0900

    いちばんやさしいPython入門教室

--- python_Introductory_classroom/Chapter_4/lesson_4-4.py
+++ python_Introductory_classroom/Chapter_4/lesson_4-4.py
@@ -8,6 +8,7 @@ while total <= 50:
 print(total)
 
 # for構文と同じ処理ををwhileで記述する
+# coding:utf-8
 for a in range(1,5+1):
     print(a)
 
@@ -15,3 +16,15 @@ a = 1
 while a <= 5:
     print(a)
     a = a + 1
+
+for i in range( 1 , 5 + 1 ):
+    print(i)
+else:
+    print("この処理を終了する")
+
+# 繰り返しが終わったときに実行する else
+a = "処理を繰り返す"
+while True :
+    print(a)
+else:
+    print("この処理を終了する")
