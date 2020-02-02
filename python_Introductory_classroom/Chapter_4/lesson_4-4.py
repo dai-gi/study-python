1c07ddee958c5a23cca89dce08d2d7413cdc2159
daisuke524 <daisuke.u524@gmail.com>
2020-02-03 07:18:02 +0900

    いちばんやさしいPython入門教室

--- python_Introductory_classroom/Chapter_4/lesson_4-4.py
+++ python_Introductory_classroom/Chapter_4/lesson_4-4.py
@@ -6,3 +6,12 @@ while total <= 50:
     total = total + a
     a = a + 1
 print(total)
+
+# for構文と同じ処理ををwhileで記述する
+for a in range(1,5+1):
+    print(a)
+
+a = 1
+while a <= 5:
+    print(a)
+    a = a + 1
