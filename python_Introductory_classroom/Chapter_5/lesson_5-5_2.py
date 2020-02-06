1e6463ae8faca65549b986e7658fd882f39dc196
daisuke524 <daisuke.u524@gmail.com>
2020-02-06 19:23:23 +0900

    いちばんやさしいPython入門教室

--- python_Introductory_classroom/Chapter_5/lesson_5-5_2.py
+++ python_Introductory_classroom/Chapter_5/lesson_5-5_2.py
@@ -1,17 +1,19 @@
 # 数当てゲームプログラムの完成形
 
-# 　coding:utf-8
+# coding:utf-8
 import random
+
 # コンピュータが考えた4桁のランダムの数値
-a = [random.randint(0,9),
+a =[random.randint(0,9),
     random.randint(0,9),
     random.randint(0,9),
     random.randint(0,9)]
-# テストの為の答えを表示
+
+# テストのための答えを表示
 print(str(a[0]) + str(a[1]) + str(a[2]) + str(a[3]))
-# 4桁の数字かどうかを判定する
-while True:
-    
+
+while True :
+# 4桁の数字かどうかを判定
     isok = False
     while isok == False:
         b = input("数を入れてね>")
@@ -20,33 +22,32 @@ while True:
         else:
             kazuok = True
             for i in range(4):
-                if (b[i] > "0") or (b[i] < "9"):
+                if (b[i] <"0") or (b[i] > "9") :
                     print("数字ではありません")
                     kazuok = False
                     break
-                if kazuok:
-                    isok = True
-# 4桁の数字だった場合　ヒットを判定
+            if kazuok :
+                isok = True
+
+# ヒットを判定
     hit = 0
     for i in range(4):
-        if a[i] == str(b[i]):
+        if a[i] == int(b[i]):
             hit = hit + 1
-# ブロー判定をする
+
+# ブロー判定
     blow = 0
     for j in range(4):
         for i in range(4):
             if (int(b[j]) == a[i]) and (a[i] != int(b[i])) and (a[j] != int(b[j])):
                 blow = blow + 1
                 break
+
 # ヒット数とブロー数を表示
-    print("ヒット"　+ str(hit))
-    print("ブロー"　+ str(blow))
+    print(" ヒット " +str(hit))
+    print(" ブロー " +str(blow))
 
-
-
-
-
-
-
-
-
+# ヒットが4になったら終了
+    if hit == 4:
+        print("当たり！")
+        break
