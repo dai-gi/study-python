23d25098e0fd39326d1a7ec6fc9117431c7eda06
daisuke524 <daisuke.u524@gmail.com>
2020-02-06 13:49:54 +0900

    エラーが出る

--- python_Introductory_classroom/Chapter_5/lesson_5-5_2.py
+++ python_Introductory_classroom/Chapter_5/lesson_5-5_2.py
@@ -3,7 +3,7 @@
 # 　coding:utf-8
 import random
 # コンピュータが考えた4桁のランダムの数値
-a =[random.randint(0,9),
+a = [random.randint(0,9),
     random.randint(0,9),
     random.randint(0,9),
     random.randint(0,9)]
@@ -14,4 +14,39 @@ while True:
     
     isok = False
     while isok == False:
-        
+        b = input("数を入れてね>")
+        if len(b) != 4:
+            print("4桁の数字を入力してください")
+        else:
+            kazuok = True
+            for i in range(4):
+                if (b[i] > "0") or (b[i] < "9"):
+                    print("数字ではありません")
+                    kazuok = False
+                    break
+                if kazuok:
+                    isok = True
+# 4桁の数字だった場合　ヒットを判定
+    hit = 0
+    for i in range(4):
+        if a[i] == str(b[i]):
+            hit = hit + 1
+# ブロー判定をする
+    blow = 0
+    for j in range(4):
+        for i in range(4):
+            if (int(b[j]) == a[i]) and (a[i] != int(b[i])) and (a[j] != int(b[j])):
+                blow = blow + 1
+                break
+# ヒット数とブロー数を表示
+    print("ヒット"　+ str(hit))
+    print("ブロー"　+ str(blow))
+
+
+
+
+
+
+
+
+
