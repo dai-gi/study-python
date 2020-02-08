fe1e19b0d1c03eb3a26e3917c36ab0010abe93c2
daisuke524 <daisuke.u524@gmail.com>
2020-02-09 01:34:30 +0900

    メッセージを表示する

---
+++ python_Introductory_classroom/Chapter_6/lesson_6-4_2.py
@@ -0,0 +1,24 @@
+# メッシージを表示する
+# coding:utf-8
+import tkinter as tk 
+import tkinter.messagebox as tmsg
+
+# ボタンがクリックされたときの処理
+def ButtonClick():
+    tmsg.showinfo("テスト", "クリックされたよ")
+
+# メインのプログラム
+root = tk.Tk()
+root.geometry("400x150")
+root.title("数当てゲーム")
+
+label1 = tk.Label(root, text=("数を入力してね"), font=("Helvetica", 25))
+label1.place(x = 20, y = 20)
+
+editbox1 = tk.Entry(width = 4, font=("Helvetica", 28))
+editbox1.place(x = 129, y = 60)
+
+button1 = tk.Button(root, text = "クリック", font=("Helvetica", 30), command=ButtonClick)
+button1.place(x = 220, y = 60)
+
+root.mainloop()
