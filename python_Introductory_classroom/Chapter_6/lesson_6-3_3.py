b3bf1fe6fc7953b7c7a81c42e7787485f60f13f7
daisuke524 <daisuke.u524@gmail.com>
2020-02-09 01:32:03 +0900

    フォントの種類とサイズを変える

--- python_Introductory_classroom/Chapter_6/lesson_6-3_3.py
+++ python_Introductory_classroom/Chapter_6/lesson_6-3_3.py
@@ -9,10 +9,7 @@ root.title("数を入力してね")
 label1 = tk.Label(root, text="数を入力してね", font=("Helvetica", 14))#　フォントの種類をサイズを変更
 label1.place(x = 20, y = 20)
 
-
-
 editbox1 = tk.Entry(root, width = 4, font=("Helvetica", 28))#　フォントの種類をサイズを変更
 editbox1.place(x = 120, y = 60)
-
 
 root.mainloop()
