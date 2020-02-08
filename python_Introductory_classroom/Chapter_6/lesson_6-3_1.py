1dab1aa29e91d0924050473f3afd0203d18d8599
daisuke524 <daisuke.u524@gmail.com>
2020-02-08 21:21:30 +0900

    ウィンドウにメッセージの位置を決めて表示する

--- python_Introductory_classroom/Chapter_6/lesson_6-3_1.py
+++ python_Introductory_classroom/Chapter_6/lesson_6-3_1.py
@@ -1,1 +1,12 @@
-# 作成したウィンドウに考えたメッセージを任意の位置に表示する方法
+# ウィンドウにメッセージの位置を決めて表示する
+#  coding:utf8
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("400x150")
+root.title("数当てゲーム")
+
+label1 = tk.Label(root, text="数を入力してね")
+label1.place(x = 20, y = 20)
+
+root.mainloop()
