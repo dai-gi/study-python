05fb0a0a4ec09a231f3d407af6c00edeb313040f
daisuke524 <daisuke.u524@gmail.com>
2020-02-13 19:08:14 +0900

    for文

---
+++ python-izm_1/16_for.py
@@ -0,0 +1,11 @@
+# for文の基礎
+for value in 'ABCDEF':
+    print(value)
+
+test_list_1 = [['https', 'www'], ['python-izm', 'com']]
+
+for value in test_list_1:
+    print(value)
+
+for value_1, value_2 in test_list_1:
+    print(value_1, value_2)
