749e8101d887e4c4c61c4807e20f294ad3194b2c
daisuke524 <daisuke.u524@gmail.com>
2020-02-13 19:18:22 +0900

    while文

---
+++ python-izm_1/17_while.py
@@ -0,0 +1,14 @@
+# while文
+counter = 0
+
+while counter < 10:
+    counter += 1
+    print(counter)
+
+counter = 0
+
+while True:
+    counter += 1
+    print(counter)
+    if counter == 30:
+        break
