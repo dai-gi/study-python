85f0d109347d67f6afe8a21a277bce303f9194ce
daisuke524 <daisuke.u524@gmail.com>
2020-02-14 23:29:12 +0900

    フォーマット出力

---
+++ python-izm_1/21_print.py
@@ -0,0 +1,7 @@
+# フォーマット出力
+print('Pythonの学習サイト : %s' % 'python-izm.com')
+print('Pythonの学習サイト : %s-%s.%s' % ('python', 'izm', 'com'))
+
+test_int = 100 + 100
+test_str = 'python-izm.com'
+print('サイトの開設 %d 日目！ %s' %(test_int, test_str))
