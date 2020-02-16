c599c7e982757b1ddb463e7ae406d75d2c1a84c0
daisuke524 <daisuke.u524@gmail.com>
2020-02-16 21:56:36 +0900

    関数

--- python-izm_2/1_function.py
+++ python-izm_2/1_function.py
@@ -1,29 +1,51 @@
 # 関数の基本
 # 引数の基礎
-# ＋ 例外処理
+# 引数の初期値
+# 関数とメソッド
+# ーーーーーーーーーーーーー
+# 例外処理
+
+
+
+# 関数
+def test_func():
+    print(1)
+
+class TestClass:
+    # メソッド
+    def test_method():
+        print(1)
+
+test_func()
+print('-----------------')
+TestClass.test_method()
+
+print('=================')
+print(type(test_func))
+print('-----------------')
+print(type(TestClass.test_method))
 
 
 
 
 
 
-def test_func(n_1, n_2, op):
-    
-    if op ==1:
-        print('足し算')
-        print(n_1 + n_2)
-    else: 
-        op ==2
-        print('引き算')
-        try:
-            print(n_1 - n_2)
-        except:
-            print('計算出来ませんでした')
-        finally:
-            print('計算終了')
+#     if op ==1:
+#         print('足し算')
+#         print(n_1 + n_2)
+#     else: 
+#         op ==2
+#         print('引き算')
+#         try:
+#             print(n_1 - n_2)
+#         except:
+#             print('計算出来ませんでした')
+#         finally:
+#             print('計算終了')
                 
     
 
-test_func(50, '50', 2)
+# test_func(50, 50)
+
 
 
