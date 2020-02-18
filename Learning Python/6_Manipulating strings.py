288cd413ebbb2503f1af9f7daace040ee9d7d0af
daisuke524 <daisuke.u524@gmail.com>
2020-02-18 23:51:35 +0900

    文字列の操作

--- Learning Python/6_Manipulating strings.py
+++ Learning Python/6_Manipulating strings.py
@@ -1,5 +1,5 @@
-# len関数
-str_len = len('Hello World')
+# len 関数
+str_len = ('Hello World')
 print(str_len)
 msg = 'Good-bye World'
 str_len = len(msg)
@@ -8,29 +8,40 @@ print(str_len)
 # インデックス
 msg = 'Hello'
 print(msg[0])
+print(msg[5])
+print(msg[4])
+print(msg[len(msg)-1])
+print(msg[-1])
 
-# 負の値
-msg = 'Hello'
-# print(msg[len(msg)-1])
-# print(msg[-1])
+msg[0] = 'h'
 
-# 文字列のスライス
-print(msg[::])
+# スライス
+print(msg[1:4])
+print(msg[:3])
+print(msg[2:])
+print(msg[:])
+print(msg[::2])
 
-# ↓インタラクティブモードに入力↓
+# 文字列の結合
+msg = 'Hello ' "World"'''!
+Good-bye'''""" World"""
+print(msg)
 
-# 'Setagaya' in 'Yoyogi, Shibuya-ku, Tokyo, Japan'
-# 'Setagaya' not in 'Yoyogi, Shibuya-ku, Tokyo, Japan'
+world = 'World'
+msg = 'Hello'world
+print(msg)
 
-# find / rfindメソッド 
-# index / rindex
-sample_str = 'find, rfind,  index, rindex'
-# print(sample_str.find('index'))
-# print(sample_str.rfind('index'))
-# print(sample_str.find('foo'))
-# print(sample_str.index('find'))
-# print(sample_str.rindex('find'))
-# print(sample_str.index('foo'))
+world = 'World'
+msg = 'Hello' + world
+print(msg)
 
-a = len(sample_str)
-print(a)
+result = '1' + 1
+print(result)
+
+# 乗算
+print('a' * 5)
+
+# find / rfind / index / rindex
+
+
+
