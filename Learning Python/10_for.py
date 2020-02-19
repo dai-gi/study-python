8aa75550a0451129eaf8e78df8cdc6c6fcf6d840
daisuke524 <daisuke.u524@gmail.com>
2020-02-19 10:24:31 +0900

    for文

---
+++ Learning Python/10_for.py
@@ -0,0 +1,15 @@
+# # 繰り返し処理とは
+# # for文
+# message = 'Hello Python'
+
+# for ch in message:
+#     print(ch)
+
+# message = ' '
+
+# for ch in message:
+#     print(ch)
+
+# # for文とrange関数による繰り返し処理
+# me
+
