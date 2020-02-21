5e2b0b6241015c6cdbd1868c0700ccb29ab5d6b2
daisuke524 <daisuke.u524@gmail.com>
2020-02-21 10:20:38 +0900

    ランダムなボタン

--- オリジナルプログラムの作成/Random Button.py
+++ オリジナルプログラムの作成/Random Button.py
@@ -1,3 +1,12 @@
 # coding:utf-8
 import random
 
+a, b, c, d, e, f, g, h, i  = 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+random.randint(a, i)
+Button1 = random.randint(a, i)
+
+print(Button1)
+
+for Button in (a, b, c, d, e, f, g, h, i):
+    print(Button1)
