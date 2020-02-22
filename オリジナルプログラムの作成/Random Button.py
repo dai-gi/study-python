fe03d7e1188ca9fddf0c5b6b3cb250e5f3875a98
daisuke524 <daisuke.u524@gmail.com>
2020-02-22 10:22:41 +0900

    ランダムなボタン

--- オリジナルプログラムの作成/Random Button.py
+++ オリジナルプログラムの作成/Random Button.py
@@ -1,13 +1,34 @@
+# ランダムに1桁の整数を選ぶプログラム
 # coding:utf-8
+# import random
+
+# a, b, c, d, e, f, g, h, i  = 1, 2, 3, 4, 5, 6, 7, 8, 9
+# Button1 = []
+
+# for Button1 in (a, b, c, d, e, f, g, h, i):
+#     print(Button1)
+
+# print('===================================================')
+# random.randint(a, i)
+# Button1 = random.randint(a, i)
+
+# print(Button1)
+
+
+# クラスを使って9個のボタンを作成する
 import random
+class NineButtons:
+    def __init__(self, a, b, c, d, e, f, g, h, i):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.e = e
+        self.f = f
+        self.g = g
+        self.h = h
+        self.i = i
 
-a, b, c, d, e, f, g, h, i  = 1, 2, 3, 4, 5, 6, 7, 8, 9
-
-for Button1 in (a, b, c, d, e, f, g, h, i):
-    print(Button1)
-
-random.randint(a, i)
-Button1 = random.randint(a, i)
-
-print(Button1)
+    def Button1(self):
+        
 
