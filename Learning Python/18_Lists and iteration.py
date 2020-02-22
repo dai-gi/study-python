bfaec96059df4ae645abd398de1b1daffd5052b0
daisuke524 <daisuke.u524@gmail.com>
2020-02-22 09:15:20 +0900

    リストと繰り返し処理

--- Learning Python/18_Lists and iteration.py
+++ Learning Python/18_Lists and iteration.py
@@ -1,10 +1,10 @@
 # # for文でのリスト処理の基本
-# intlist = [44, 99, 78, 36, 86, 100, 89, 48, 50, 58]
-# result = intlist[0] + intlist[1] + intlist[2] + intlist[3] + intlist[4] +\
-#         intlist[5] + intlist[6] + intlist[7] + intlist[8] + intlist[9]
-# print(result)
+intlist = [44, 99, 78, 36, 86, 100, 89, 48, 50, 58]
+result = intlist[0] + intlist[1] + intlist[2] + intlist[3] + intlist[4] +\
+        intlist[5] + intlist[6] + intlist[7] + intlist[8] + intlist[9]
+print(result)
 
-# result = 0
+result = 0
 
 # for number in intlist:
 #     result = result + number
@@ -82,22 +82,22 @@
 # print(list(result))
 
 # all関数とany関数
-intlist1 = list(range(5))  # [0, 1, 2, 3, 4]
-intlist2 = list(range(1, 6))  # [1, 2, 3, 4, 5]
-intlist3 = [0] * 5  # [0, 0, 0, 0, 0]
-strlist1 = ['', 'foo', 'bar']
-strlist2 = ['foo', 'bar', 'baz']
-strlist3 = [''] * 5
-emptylist = []
+# intlist1 = list(range(5))  # [0, 1, 2, 3, 4]
+# intlist2 = list(range(1, 6))  # [1, 2, 3, 4, 5]
+# intlist3 = [0] * 5  # [0, 0, 0, 0, 0]
+# strlist1 = ['', 'foo', 'bar']
+# strlist2 = ['foo', 'bar', 'baz']
+# strlist3 = [''] * 5
+# emptylist = []
 
-print('all(intlist1):', all(intlist1))  # False：0は「偽」と見なされる
-print('all(intlist2):', all(intlist2))  # True
-print('all(intlist3):', all(intlist3))  # False：0は「偽」と見なされる
+# print('all(intlist1):', all(intlist1))  # False：0は「偽」と見なされる
+# print('all(intlist2):', all(intlist2))  # True
+# print('all(intlist3):', all(intlist3))  # False：0は「偽」と見なされる
 
-print('any(strlist1):', any(strlist1))  # True
-print('any(strlist2):', any(strlist2))  # True
-print('any(strlist3):', any(strlist3))  # False：空文字列は「偽」と見なされる
+# print('any(strlist1):', any(strlist1))  # True
+# print('any(strlist2):', any(strlist2))  # True
+# print('any(strlist3):', any(strlist3))  # False：空文字列は「偽」と見なされる
 
-print('all(emptylist):', all(emptylist))  # True：空のリストを渡すと戻り値はTrue
-print('any(emptylist):', any(emptylist))  # False：空のリストを渡すと戻り値はFalse
+# print('all(emptylist):', all(emptylist))  # True：空のリストを渡すと戻り値はTrue
+# print('any(emptylist):', any(emptylist))  # False：空のリストを渡すと戻り値はFalse
 
