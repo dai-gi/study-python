4f023f6e98456ff44283bfbd4174e302b5052df3
daisuke524 <daisuke.u524@gmail.com>
2020-02-24 17:19:35 +0900

    ランダムなボタン

--- オリジナルプログラムの作成/Random Button.py
+++ オリジナルプログラムの作成/Random Button.py
@@ -2,6 +2,7 @@
 # coding:utf-8
 
 import random
+import tkinter as tk
 
 # classで「Buttons関数」を作成
 class Buttons:
@@ -9,14 +10,53 @@ class Buttons:
         self.x = random
         print(self.x)
 
-# 9個の変数に整数を
+# 9個の変数に9個の整数を代入する
 a, b, c, d, e, f, g, h, i  = 1, 2, 3, 4, 5, 6, 7, 8, 9
 
+# for文を使用してButton1に整数の入った9個の変数を代入をする
 for Button1 in (a, b, c, d, e, f, g, h, i):
     pass
+
+# 9個の変数をランダムに選ぶ
 Button1 = random.randint(a, i)
-# print(Button1)
 
 print('===================================')
-
 Buttons(Button1)
+
+# ウィンドウを作成する
+root = tk.Tk()
+root.geometry('500x500')
+root.title('Random Button')
+
+# ラベルを作成
+label1 = tk.Label(root, text="ボタンを押して下さい", font = ('Helvetica', 28))
+label1.place(x = 105, y = 35)
+
+# 9個のボタンを作成
+# 1段目
+button1 = tk.Button(root, text = ' 1 ', font=('Helvetica',80))
+button1.place(x = 100, y = 130)
+button1 = tk.Button(root, text = ' 2 ', font=('Helvetica',80))
+button1.place(x = 200, y = 130)
+button1 = tk.Button(root, text = ' 3 ', font=('Helvetica',80))
+button1.place(x = 300, y = 130)
+
+# 2段目
+button1 = tk.Button(root, text = ' 4 ', font=('Helvetica',80))
+button1.place(x = 100, y = 230)
+button1 = tk.Button(root, text = ' 5 ', font=('Helvetica',80))
+button1.place(x = 200, y = 230)
+button1 = tk.Button(root, text = ' 9 ', font=('Helvetica',80))
+button1.place(x = 300, y = 230)
+
+# 3段目
+button1 = tk.Button(root, text = ' 7 ', font=('Helvetica',80))
+button1.place(x = 100, y = 330)
+button1 = tk.Button(root, text = ' 8 ', font=('Helvetica',80))
+button1.place(x = 200, y = 330)
+button1 = tk.Button(root, text = ' 9 ', font=('Helvetica',80))
+button1.place(x = 300, y = 330)
+
+
+# ウィンドウを表示する
+root.mainloop()
