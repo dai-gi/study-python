f1261b32ee8ef44c4642a2ddd40ef8459b9f1ff7
daisuke524 <daisuke.u524@gmail.com>
2020-03-03 09:15:44 +0900

    4つのボタン

--- オリジナルプログラムの作成/Four Buttons.py
+++ オリジナルプログラムの作成/Four Buttons.py
@@ -15,28 +15,27 @@ class Application(tk.Frame):
         
         # ラベルを作成
         self.label1 = tk.Label(master, text="ボタン当ててね", font = ('Helvetica', 35))
-        self.label1.place(x = 141, y = 45)
+        self.label1.place(x = 70, y = 15)
 
         # 履歴を表示
         self.rirekibox = tk.Text(master, font=("Helvetica", 14))
-        self.rirekibox.place(x=325, y=125, width=90, height=200)
+        self.rirekibox.place(x=255, y=75, width=90, height=200)
 
 
         # ４つのボタン
         self.button1 = tk.Button(master, text = ' 1 ', font=('Helvetica',80),command=partial(self.click_button,'1'))
-        self.button1.place(x = 116, y = 124)
+        self.button1.place(x = 46, y = 74)
         self.button2 = tk.Button(master, text = ' 2 ', font=('Helvetica',80),command=partial(self.click_button,'2'))
-        self.button2.place(x = 220, y = 124)
-        self.button3 = tk.Button(master, text = ' 4 ', font=('Helvetica',80),command=partial(self.click_button,'3'))
-        self.button3.place(x = 220, y = 229)
-        self.button4 = tk.Button(master, text = ' 3 ', font=('Helvetica',80),command=partial(self.click_button,'4'))
-        self.button4.place(x = 116, y = 229)
+        self.button2.place(x = 150, y = 74)
+        self.button3 = tk.Button(master, text = ' 4 ', font=('Helvetica',80),command=partial(self.click_button,'4'))
+        self.button3.place(x = 150, y = 179)
+        self.button4 = tk.Button(master, text = ' 3 ', font=('Helvetica',80),command=partial(self.click_button,'3'))
+        self.button4.place(x = 46, y = 179)
         
         # ボタンが押された時の処理
     def click_button(self,num):
 
-        self.random = random.randint(1, 3)
-        seikai = "正解"
+        self.random = random.randint(1, 4)
 
         # 押下したボタンを判定
         if self.random == int(num):
@@ -44,16 +43,25 @@ class Application(tk.Frame):
             self.rirekibox.insert(tk.END, '  ' + num + ' : ' + "正解 " + "\n" )
         elif self.random != int(num):
             self.rirekibox.insert(tk.END, '  ' + num + ' : ' + "不正解 " + "\n" )
-            tmsg.showinfo("不正解")
+            tmsg.showerror("error message", "不正解")
         else:
-            if seikai == seikai:
-                self.destroy()
+            pass
+                
 
 
 # ウィンドウを作成する
 root = tk.Tk()
-root.geometry('550x400')
+# root.wm_attributes("-transparent", True)
+root.geometry('400x350')
 root.title('Random Button')
+
+
+# pyui.init(400,300)
+# pyui.run()
+
+# f = tk.Frame(root, width=550, height=400)
+# f.configure(bg="systemTransparent")
+# f.pack()
 app = Application(master=root)
 app.mainloop()
 
