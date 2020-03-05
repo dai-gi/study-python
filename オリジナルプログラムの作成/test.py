f062ecac0dd28e1375faab95e7f7e591554b6e9c
daisuke524 <daisuke.u524@gmail.com>
2020-03-05 21:39:42 +0900

    テスト

---
+++ オリジナルプログラムの作成/test.py
@@ -0,0 +1,21 @@
+# ランダムに1桁の整数を選ぶプログラム
+# coding:utf-8
+import tkinter as tk
+
+class Mainwindow(tk.Frame):
+
+    def __init__(self, master = None, *args, **kwargs):
+        super().__init__(master)
+        tk.Button(self, text = 'click me! (Open dialog)',
+                command = self.open_dialog).pack()
+
+    def open_dialog(self):
+        u""" Tk() と同じような感じで使える """
+        sub_win = tk.Toplevel(master = self.master)
+        tk.Button(sub_win, text = 'thank you', command = sub_win.destroy).pack()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    mainwindow = Mainwindow(master = root)
+    mainwindow.pack()
+    root.mainloop()
