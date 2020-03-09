fc0414c820120a68c12bfd81bb7a74b48f2c8e03
daisuke524 <daisuke.u524@gmail.com>
2020-03-09 23:42:08 +0900

    テスト

--- オリジナルプログラム/test.py
+++ オリジナルプログラム/test.py
@@ -1,21 +1,26 @@
-# ランダムに1桁の整数を選ぶプログラム
-# coding:utf-8
-import tkinter as tk
+# # ランダムに1桁の整数を選ぶプログラム
+# # coding:utf-8
+# import tkinter as tk
 
-class Mainwindow(tk.Frame):
+# class Mainwindow(tk.Frame):
 
-    def __init__(self, master = None, *args, **kwargs):
-        super().__init__(master)
-        tk.Button(self, text = 'click me! (Open dialog)',
-                command = self.open_dialog).pack()
+#     def __init__(self, master = None, *args, **kwargs):
+#         super().__init__(master)
+#         tk.Button(self, text = 'click me! (Open dialog)',
+#                 command = self.open_dialog).pack()
 
-    def open_dialog(self):
-        u""" Tk() と同じような感じで使える """
-        sub_win = tk.Toplevel(master = self.master)
-        tk.Button(sub_win, text = 'thank you', command = sub_win.destroy).pack()
+#     def open_dialog(self):
+#         u""" Tk() と同じような感じで使える """
+#         sub_win = tk.Toplevel(master = self.master)
+#         tk.Button(sub_win, text = 'thank you', command = sub_win.destroy).pack()
 
-if __name__ == '__main__':
-    root = tk.Tk()
-    mainwindow = Mainwindow(master = root)
-    mainwindow.pack()
-    root.mainloop()
+# if __name__ == '__main__':
+#     root = tk.Tk()
+#     mainwindow = Mainwindow(master = root)
+#     mainwindow.pack()
+#     root.mainloop()
+
+
+
+# coding: utf-8
+
