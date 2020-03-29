9cc5afc1c937c42cb7e2bce969f17e6e18993038
daisuke524 <daisuke.u524@gmail.com>
2020-03-29 23:10:29 +0900

    変更

--- Python-izm/python-izm_1/11_import.py
+++
@@ -1,8 +0,0 @@
-# 「testmod.py」ファイルを参照
-
-# 別名でインポート
-from datetime import datetime
-datetime.today()
-
-from datetime import datetime as d
-d.today()
--- Python-izm/python-izm_1/19_continue.py
+++
@@ -1,6 +0,0 @@
-# continue
-for num in range(100):
-
-    if num % 10:
-        continue
-    print(num)
