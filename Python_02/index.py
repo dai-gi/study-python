a81c0f87d167fb9bb075a381f8832dec7e28a785
daisuke524 <daisuke.u524@gmail.com>
2020-05-09 00:39:49 +0900

    フォルダ名変更

---
+++ Python_02/index.py
@@ -0,0 +1,7 @@
+scores = {'network':60, 'database':80, 'security':60}
+print(list(scores))
+print(tuple(scores))
+print(set(scores))
+print(list(scores.values()))
+print(tuple(scores.values()))
+print(set(scores.values()))
