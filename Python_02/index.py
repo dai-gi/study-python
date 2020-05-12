b3eaed9b921f2c333e54aa4a0343963f94b0eb31
daisuke524 <daisuke.u524@gmail.com>
2020-05-12 23:27:40 +0900

    編集

--- Python_02/index.py
+++
@@ -1,7 +0,0 @@
-scores = {'network':60, 'database':80, 'security':60}
-print(list(scores))
-print(tuple(scores))
-print(set(scores))
-print(list(scores.values()))
-print(tuple(scores.values()))
-print(set(scores.values()))
