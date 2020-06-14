09d3bb257a8c465c494924886ed23dfb3cf42598
daisuke524 <daisuke.u524@gmail.com>
2020-06-14 17:31:20 +0900

    数値の演算・論理値・文字列の演算

---
+++ myvenv/dotinstall/myapp_#05.py
@@ -0,0 +1,21 @@
+# 数値の演算
+x = 10
+print(x / 3) # 3.33.... 　割り算
+print(x // 3) # 3 　切捨て除算
+print(x % 3) # 1 　余りを求める
+print(x ** 2) # 100  べき乗
+
+y = 4
+# y = y + 12
+y += 12 # 同じ答えになる
+print(16)
+
+# 論理値
+# and or not
+print(True and False) # False
+print(True or False) # True
+print(not True) # False
+
+# 文字列の演算
+print("hello " + "world")
+print("hello " * 3)
