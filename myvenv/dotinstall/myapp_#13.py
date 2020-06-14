6f5c6fa2c1badc033513d337dc3e89bf6911f875
daisuke524 <daisuke.u524@gmail.com>
2020-06-14 22:40:50 +0900

    クラス

---
+++ myvenv/dotinstall/myapp_#13.py
@@ -0,0 +1,18 @@
+# クラス
+
+user_name = "daisuke"
+user_score = 10
+
+class User:
+    pass
+
+tom = User() # Userクラスのインスタンス
+tom.name = "tom"
+tom.score = 20
+
+bob = User() # Userクラスのインスタンス
+bob.name = "bob"
+bob.level = 5
+
+print(bob.name)
+print(tom.score)
