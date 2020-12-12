bc7308e6c473b2abbcdbf58f3a5283ce3eb831ab
daisuke524 <daisuke.u524@gmail.com>
2020-12-12 12:34:01 +0900

    移動

---
+++ Function/map.py
@@ -0,0 +1,19 @@
+# # map関数について
+
+"""
+イテラブル（リストなどのオブジェクト）の全てに対して、
+map関数の第一引数で指定した関数を適用させることができる。
+"""
+li1 = [1, 2, 3, 4, 5]
+print(type(li1[0])) # <class 'int'>
+
+# リスト内の要素を全て文字列に変換する。
+li_str = list(map(str, li1))
+print(type(li_str[0])) # <class 'str'>
+print('------------------')
+
+# リスト関数を使わなかった場合、エラーになってしまう。
+li2 = [1, 2, 3, 4, 5]
+li_str = map(str, li2)
+print(type(li_str[0])) # TypeError: 'map' object is not subscriptable
+print('------------------')
