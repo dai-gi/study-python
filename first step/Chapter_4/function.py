b53fc8dc52d47f7fbbf36b6a4432662390f7030a
daisuke524 <daisuke.u524@gmail.com>
2020-12-13 17:14:29 +0900

    修正

--- first step/Chapter_4/function.py
+++ first step/Chapter_4/function.py
@@ -1,61 +1,32 @@
-# 関数を定義する
+# いちばんやさしいPython入門教室
+# 関数を定義
+# def tashizan(a, b):
+#     total = 0
+#     for i in range(a, b + 1):
+#         total = total + i
+#     return total
+# result = tashizan(1, 2)
+# print(result)
+# print('-----------')
 
-# coding:utf-8
-def tashizan (a, b ):
-    total = 0
-    for i in range(a, b + 1):
-        total = total + i
-    return total
+# # スコープを理解
+# # test関数内で、変数aに再代入することはできない
+# a = "abc" # <- グローバル変数(グローバルスコープ)
+# def test():
+#     a = 1 # <- ローカル変数(ローカルスコープ)　＊関数ないで新しい変数aを生成することになる
+#     print(a)
+#     return
 
-# 関数を利用する
-
-# coding:utf-8
-def tashizan(a, b):
-    total = 0
-    for i in range(a, b + 1):
-        total = total + i
-    return total
-
-c = tashizan(1, 5)
-print(c)
-
-# スコープを理解する
-
-# coding:utf-8
-
-a = "abc"
-
-def test():
-    print(a)
-    return
-
-test()
-print(a)
-
-
-# グローバルスコープとローカルスコープ
-# coding:utf-8
-
-a = "abc"
-
-def test():
-    a = "def"
-    print(a)
-    return
-
-test()
-print(a)
+# test()
+# print(a)
 
 # 関数からグローバルスコープにアクセスする
-# coding:utf-8
-
 a = "abc"
 
 def test():
-    global a
-    a = "def"
-    print(a)
-    return
+    global a # グローバル変数にアクセスする場合は、「global」と宣言する必要がある
+    global a = "def"
+    return a
 
 test()
 print(a)
