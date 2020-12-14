c2fc19e449cb4fb651d461659d707a52ae00c4f0
daisuke524 <daisuke.u524@gmail.com>
2020-12-15 07:11:37 +0900

    修正

--- first step/Chapter_5/lesson_5-3.py
+++ first step/Chapter_5/lesson_5-3.py
@@ -1,4 +1,5 @@
-# 4桁のランダムな値を作る
+# いちばんやさしいPython入門教室
+# 4桁のランダムな値を作成
 # coding:utf-8
 
 import random
@@ -8,12 +9,10 @@ import random
 # a3 = random.randint(0,9)
 # a4 = random.randint(0,9)
 
-# 上記のランダムな数値の変数をリストを使って作りなをす為、上記をコメントアウトした
-
-# 教材通りに記述
+# 上記をリスト化
 a = [random.randint(0, 9),
      random.randint(0, 9),
      random.randint(0, 9),
      random.randint(0, 9)]
 
-print(str(a[0]) + str(a[1]) + str(a[2]) + str(a[3])) #上記変更の為、変数の数値を変更
+print(str(a[0]) + str(a[1]) + str(a[2]) + str(a[3]))
