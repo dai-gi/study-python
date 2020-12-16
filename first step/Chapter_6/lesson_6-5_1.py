7b5f6e2679d08dadefdd3434aafccabef47c0f1b
daisuke524 <daisuke.u524@gmail.com>
2020-12-16 12:37:39 +0900

    修正

--- first step/Chapter_6/lesson_6-5_1.py
+++ first step/Chapter_6/lesson_6-5_1.py
@@ -1,5 +1,7 @@
+# いちばんやさしいPython入門教室
 # 入力されたテキスト値を取得する
 # coding:utf-8
+
 import tkinter as tk
 import tkinter.messagebox as tmsg
 
@@ -9,23 +11,17 @@ def ButtonClick():
     # メッセージとして表示する
     tmsg.showinfo("入力されたテキスト", b)
 
-# メインのプログラム
-# ウィンドウを作る
 root = tk.Tk()
 root.geometry("400x150")
 root.title("数当てゲーム")
 
-# ラベルを作る
 label1 = tk.Label(root, text = "数を入力してね", font=("Helvetica", 25))
 label1.place(x = 20, y = 20)
 
-# テキストボックスを作る
 editbox1 = tk.Entry(width = 4, font=("Helvetica", 20))
 editbox1.place(x = 120, y = 60)
 
-# ボタンを作る
 button1 = tk.Button(root, text = "クリック", font=("Helvetica", 30), command=ButtonClick)
 button1.place(x = 220, y = 60)
 
-# ウィンドウを表示する
 root.mainloop()
