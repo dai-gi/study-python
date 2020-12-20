e4d1825d998bceba7c07d3a454f323cd3b9562a2
dai-gi <daisuke.u524@gmail.com>
2020-12-20 21:45:14 +0900

    修正

--- Python-izm/python-izm_1/dictionary.py
+++ Python-izm/python-izm_1/dictionary.py
@@ -1,70 +1,69 @@
-# ディクショナリの基本
-test_dict_1 = {'YEAR':'2010', 'MONTH':'1', 'DAY':'20'}
-print(test_dict_1)
+# ディクショナリ
+dict_1 = {'YEAR':'2010', 'MONTH':'1', 'DAY':'20'}
+print(dict_1) # {'YEAR': '2010', 'MONTH': '1', 'DAY': '20'}
 
-print('=================================')
-for i in test_dict_1:
-    print(i)
-    print(test_dict_1[i])
-    print('-------------------------------')
+# for文でディクショナリの「key」と「value」を取り出す
+# for i in dict_1:
+#     print(i) # <- key
+#     # ↑↑ 出力結果：'YEAR'
+#     print(dict_1[i]) # <- [i] key を指定して value
+#     # ↑↑ 出力結果：'2010'
+print('------------------')
 
 
-# valueの取得
-test_dict_1 = {'YEAR':'2010', 'MONTH':'1', 'DAY':'20'}
-print(test_dict_1)
-print('=================================')
-print(test_dict_1['YEAR'])
-print(test_dict_1['YEARS'])
-print('---------------------------------')
-print(test_dict_1.get('YEAR'))
-print(test_dict_1.get('YEARS'))
-print('---------------------------------')
-print(test_dict_1.get('YEAR', 'NOT FOUND'))
-print(test_dict_1.get('YEARS', 'NOT FOUND'))
+# getメソッド
+print(dict_1.get('YEAR')) # '2010' 
 
+print(dict_1.get('YEAES')) # None
+
+print(dict_1.get('YEAR', 'NOT FOUND')) # '2010'
+
+print(dict_1.get('YEARS', 'NOT FOUND')) # 'NOT FOUND'
+                            # ↑↑ 第二引数に存在しなかった場合の返り値を指定できる
+print('------------------')
 
 # 要素の追加
-test_dict_1 = {}
-print(test_dict_1)
+print(dict_1) # {'YEAR': '2010', 'MONTH': '1', 'DAY': '20'}
+dict_1['HAY'] = 'hi'
+print(dict_1) # {'YEAR': '2010', 'MONTH': '1', 'DAY': '20', 'HAY': 'hi'}
+print('------------------')
 
-print('=================================')
-test_dict_1['YEAR'] = '2010'
-test_dict_1['MONTH'] = '1'
-test_dict_1['DAY'] = '20'
-print(test_dict_1)
+# delメソッド(要素の削除)
+print(dict_1) # {'YEAR': '2010', 'MONTH': '1', 'DAY': '20', 'HAY': 'hi'}
+del dict_1['HAY']
+print(dict_1) # {'YEAR': '2010', 'MONTH': '1', 'DAY': '20'}
+print('------------------')
 
+# keyメソッド
+print(dict_1.keys()) # dict_keys(['YEAR', 'MONTH', 'DAY'])
 
-# 要素の削除
-test_dict_1 = {'YEAR':'2010', 'MONTH':'1', 'DAY':'20'}
-print(test_dict_1)
-
-print('=================================')
-del test_dict_1['DAY']
-print(test_dict_1)
-
-
-# keyやvalueだけを取得する
-test_dict_1 = {'YEAR':'2010', 'MONTH':'1', 'DAY':'20'}
-print(test_dict_1)
-
-print('=================================')
-print(test_dict_1.keys())
-print(test_dict_1.values())
-
+# valueメソッド
+print(dict_1.values()) # dict_values(['2010', '1', '20'])
+print('------------------')
 
 # keyとvalueを同時に取得する
-test_dict_1 = {'YEAR':'2010', 'MONTH':'1', 'DAY':'20'}
-print(test_dict_1)
-
-print('=================================')
-for key, value in test_dict_1.items():
+for key, value in dict_1.items():
     print(key, value)
+"""
+出力結果：
+YEAR 2010
+MONTH 1
+DAY 20
+"""
 
+# itemメソッド
+print(dict_1.items()) # dict_items([('YEAR', '2010'), ('MONTH', '1'), ('DAY', '20')])
 
-# keyを保持しているのか確認
-test_dict_1 = {'YEAR':'2010', 'MONTH':'1', 'DAY':'20'}
-print(test_dict_1)
+print(type(dict_1.items())) # <class 'dict_items'>
 
-print('=================================')
-print('YEAR' in test_dict_1)
-print('YEARS' in test_dict_1)
+# 値を使って何か処理をしたい場合の方法
+
+# list関数を使う
+print(list(dict_1.items())) # [('YEAR', '2010'), ('MONTH', '1'), ('DAY', '20')]
+                                    # ↑↑ リストの値はタプル
+list_dict_1 = list(dict_1.items())
+print(type(list_dict_1[0])) # <class 'tuple'>
+
+# 指定した key が入っているか確認する
+print('YEAR' in dict_1) # True
+print('YEARS' in dict_1) # False
