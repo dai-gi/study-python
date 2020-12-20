ef86a25bd8a62f4f02721f0feba4597e97305082
dai-gi <daisuke.u524@gmail.com>
2020-12-20 20:25:55 +0900

    コメントを修正

--- Collection/collection.py
+++ Collection/collection.py
@@ -1,31 +1,31 @@
 scores = {'network':60, 'database':80, 'security':60}
 
-# リストに変換 
-# print(list(scores)) # ['network', 'database', 'security']
-# # ディクショナリの値をリスト化する
-# print(list(scores.values())) # [60, 80, 60]
-# print('-----------')
+リストに変換 
+print(list(scores)) # ['network', 'database', 'security']
+# ディクショナリの値をリスト化する
+print(list(scores.values())) # [60, 80, 60]
+print('-----------')
 
 
-# # タプルに変換
-# print(tuple(scores)) # ('network', 'database', 'security')
-# # ディクショナリの値をタプル化する
-# print(tuple(scores.values())) # ['network', 'database', 'security']
-# print('-----------')
+# タプルに変換
+print(tuple(scores)) # ('network', 'database', 'security')
+# ディクショナリの値をタプル化する
+print(tuple(scores.values())) # ['network', 'database', 'security']
+print('-----------')
 
 
-# # セットに変換
-# print(set(scores)) # {'network', 'security', 'database'}
-# # ディクショナリの値をセット化する
-# print(set(scores.values())) # ['network', 'database', 'security']
-# print('-----------')
+# セットに変換
+print(set(scores)) # {'network', 'security', 'database'}
+# ディクショナリの値をセット化する
+print(set(scores.values())) # ['network', 'database', 'security']
+print('-----------')
 
 
 # ２つのリストを使って、ディクショナリを生成する
-# key = list(scores) # ['network', 'database', 'security']
-# value = list(scores.values()) # [60, 80, 60]
-# print(dict(zip(key, value))) # {'network':60, 'database':80, 'security':60}
-# print('-----------')
+key = list(scores) # ['network', 'database', 'security']
+value = list(scores.values()) # [60, 80, 60]
+print(dict(zip(key, value))) # {'network':60, 'database':80, 'security':60}
+print('-----------')
 
 
 # コレクションのネスト
