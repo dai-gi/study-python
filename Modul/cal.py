1c3a55b42133efc1b5bbc7fcf7aa1ccad1b7fa86
dai-gi <daisuke.u524@gmail.com>
2020-12-20 20:27:19 +0900

    コメントを修正

--- Modul/cal.py
+++ Modul/cal.py
@@ -1,17 +1,17 @@
 # いちばんやさしいPython入門教室
 # calendarモジュール
 
-# # from
-# from calendar import month # <- 「from」を使うとモジュール名を書かずにメソッドを使うことが出来るようになる
-# print(month(2020,1))
+# from
+from calendar import month # <- 「from」を使うとモジュール名を書かずにメソッドを使うことが出来るようになる
+print(month(2020,1))
 
-# # as
-# import calendar as cal # <- 「as」を使うとモジュール名を変更する事ができる
-# print(cal.month(2020,1))
+# as
+import calendar as cal # <- 「as」を使うとモジュール名を変更する事ができる
+print(cal.month(2020,1))
 
 
 # Python-izm
-# isleap = その年がうるう年か判定する
+# isleapメソッド = その年がうるう年か判定する
 # 返り値は「boolean型」
 
 import calendar as cal
@@ -22,6 +22,6 @@ print(cal.isleap(2016)) # True
 
 print(cal.isleap(2017)) # False
 
-# leapdays = 指定期間内にうるう年が何回あるかをカウントする
+# leapdaysメソッド = 指定期間内にうるう年が何回あるかをカウントする
 # 返り値はカウント数
 print(cal.leapdays(2010, 2020)) # 2
