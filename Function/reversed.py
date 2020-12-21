a25966bf8f72aed178fa0764dfad462ca57bbf92
dai-gi <daisuke.u524@gmail.com>
2020-12-21 22:35:07 +0900

    first commit

---
+++ Function/reversed.py
@@ -0,0 +1,17 @@
+# range関数でカウントする値を逆順にする
+for i in range(1, 5+1):
+    print(i)
+# 1
+# 2
+# 3
+# 4
+# 5
+print('-----------')
+
+for i in reversed(range(1, 5+1)):
+    print(i)
+# 5
+# 4
+# 3
+# 2
+# 1
