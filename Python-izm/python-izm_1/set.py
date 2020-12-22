6d015f2ea30a4d98e93cdbe049ceddebd3737625
dai-gi <daisuke.u524@gmail.com>
2020-12-22 21:43:44 +0900

    修正

--- Python-izm/python-izm_1/set.py
+++ Python-izm/python-izm_1/set.py
@@ -1,37 +1,46 @@
-# セットの基本
-test_set_1 = {'python', '-', 'izm', '.', 'com'}
-print(test_set_1)
+# セット
+test_set1 = {'python', '-', 'izm', '.', 'com'}
+print(test_set1) # {'izm', 'python', '-', 'com', '.'} <- 順番で出力されない
+print('-----------')
 
-print('--------------------------------------')
-for i in test_set_1:
-    print(i)
-
-test_dict = {}
-test_set = {'python'}
-empty_set = set()
-
-test_set_1 = {'python', '-', 'izm', '.', 'com', 'python', 'izm'}
-print(test_set_1)
-
-print('--------------------------------------')
-for i in test_set_1:
-    print(i)
+# ↓↓ 下記の {'python'} の様に記述し、代入すると「set」になる  
+test_set2 = {'python'}
+test_set3 = {1}
+print(type(test_set2)) # <class 'set'>
+print(type(test_set3)) # <class 'set'>
+print('-----------')
 
 
-# 要素の追加
-test_set_1 = set()
-test_set_1.add('python')
-test_set_1.update({'-', 'izm', '.', 'com'})
-print(test_set_1)
+# set関数
+set_func = set('python') # {'o', 'y', 'n', 'p', 't', 'h'}
+print(set_func)
+print('-----------')
 
 
-# 要素の削除
-test_set_1 = {'python', '-', 'izm', '.', 'com'}
-test_set_1.remove('-')
-test_set_1.discard('.')
-print(test_set_1)
+# addメソッド・updateメソッド(要素の追加)
+test_set2.add('pandas')
+print(test_set2) # {'python', 'pandas'}
+
+test_set3.update({2, 3})
+print(test_set3) # {1, 2, 3}
+print('-----------')
+
+
+# removeメソッド・discardメソッド(要素の削除)
+test_set1.remove('-')
+print(test_set1) # {'.', 'izm', 'com', 'python'}
+
+test_set1.discard('.')
+print(test_set1) # {'izm', 'com', 'python'}
+print('-----------')
 
 
 # frozenset
-test_set_1 = frozenset({'python', '-', 'izm', '.', 'com'})
-print(test_set_1)
+test_set4 = frozenset({'python', '-', 'izm', '.', 'com'})
+print(type(test_set4)) # <class 'frozenset'>
+print(test_set4) # frozenset({'-', 'python', 'izm', '.', 'com'})
+
+# 定義すると要素を操作できなくなる
+test_set4.remove('-') # AttributeError: 'frozenset' object has no attribute 'remove'
+
+
