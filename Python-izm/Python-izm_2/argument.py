89342b865f3833d5a8d4ef49044b760b2b42a9ee
dai-gi <daisuke.u524@gmail.com>
2020-12-26 17:57:26 +0900

    修正

--- Python-izm/Python-izm_2/argument.py
+++ Python-izm/Python-izm_2/argument.py
@@ -1,13 +1,32 @@
+# Python-izm｜可変長引数 
+
 # *args
-# **kwargs
-
-
-def test_func(**args):
+def test_args1(*args):
     print(args)
 
-code = 100
-name = 'python-izm'
+test_args1(1, 2, 3, 4, 5) # (1, 2, 3, 4, 5)
+print('-----------')
+print('')
 
-test_func(code,name)
-# test_func(1)
+def test_args2(code, name, *args):
+    print(code, name)
+    print(args)
 
+test_args2(100, 'python-izm', 'JP', 'US')
+
+"""""""""""""""""
+出力結果：
+
+100 python-izm
+('JP', 'US')
+
+"""""""""""""""""
+print('-----------')
+print('')
+
+
+# **kwargs
+def test_args2(**kwargs):
+    print(kwargs)
+
+test_args2(email='xxx.com', city='Tokyo') # {'email': 'xxx.com', 'city': 'Tokyo'}
