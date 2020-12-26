c4a42b9598501564c38ddde7dde17728c805bc1d
dai-gi <daisuke.u524@gmail.com>
2020-12-26 10:23:28 +0900

    修正

--- Python-izm/python-izm_2/Class.py
+++ Python-izm/python-izm_2/Class.py
@@ -1,3 +1,4 @@
+# Python-izm｜クラス作成
 class TestClass:
 
     def __init__(self, code, name):
@@ -6,10 +7,22 @@ class TestClass:
 
 
 classes = []
-classes.append(TestClass(1, 'テスト1'))
-classes.append(TestClass(2, 'テスト2'))
+classes.append(TestClass(1, 'テスト1')) # <- インスタンス化
+classes.append(TestClass(2, 'テスト2')) # <- インスタンス化
 
 for test_cls in classes:
     print('===== Class =====')
     print(test_cls.code)
     print(test_cls.name)
+
+"""""""""""""""""""""""
+出力結果：
+
+===== Class =====
+1
+テスト1
+===== Class =====
+2
+テスト2
+
+"""""""""""""""""""""""
