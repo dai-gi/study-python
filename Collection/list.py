b028f5b9cc44bd86f07c1f3ac2ecd84789f98882
dai-gi <daisuke.u524@gmail.com>
2020-12-31 23:53:28 +0900

    @IT｜[Python入門] リストの基本の学習内容を追加

--- Collection/list.py
+++ Collection/list.py
@@ -44,3 +44,46 @@ print('------------------')
 list_2 = ['100', '200', '300', '200', '100']
 print(list_2.count('200')) # 2
 print('------------------')
+
+
+# @IT｜[Python入門] リストの基本
+# list関数でリストを定義
+strlist = list('Python') # ['P', 'y', 't', 'h', 'o', 'n']
+print(strlist)
+intlist = list(range(10)) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(intlist)
+somelist = list(intlist) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(somelist)
+
+# 様々な型の要素をリストに格納できる
+somelist = ['python', 1, 100.5]
+print(somelist) # ['python', 1, 100.5]
+
+# リストの要素をスライスで取得
+intlist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(intlist[1:4]) # [1, 2, 3]
+print(intlist[:3]) # [0, 1, 2]
+print(intlist[5:]) # [5, 6, 7, 8, 9]
+print(intlist[1:9:2]) # [1, 3, 5, 7]
+
+# リストの要素の変更
+languages = ['Python', 'Ruby', 'PHP']
+print(languages) # ['Python', 'Ruby', 'PHP']
+languages[2] = 'JabaScript'
+print(languages) # ['Python', 'Ruby', 'JabaScript']
+
+# スライスで要素を変更
+intlist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+intlist[0:5] = [10, 11, 12, 13, 14]
+print(intlist)  # [10, 11, 12, 13, 14, 5, 6, 7, 8, 9]
+intlist[0:2] = [20, 21, 22]
+print(intlist)  # [20, 21, 22, 12, 13, 14, 5, 6, 7, 8, 9]
+intlist[0:6] = [0, 1, 2]
+print(intlist)  # [0, 1, 2, 5, 6, 7, 8, 9]
+
+# リストの結合
+cs = 'C＃'
+number = 99
+print(languages) # ['Python', 'Ruby', 'JabaScript']
+languages = languages + [cs, number]
+print(languages) # ['Python', 'Ruby', 'JabaScript', 'C＃', 99]
