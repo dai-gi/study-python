d33d2531fcce785998e34cb64f2dcfd29a026000
dai-gi <daisuke.u524@gmail.com>
2021-01-01 18:51:24 +0900

    first commit

---
+++ Function/tuple_func.py
@@ -0,0 +1,6 @@
+# タプルに変換
+scores = {'network':60, 'database':80, 'security':60}
+print(tuple(scores)) # ('network', 'database', 'security')
+# ディクショナリの値をタプル化する
+print(tuple(scores.values())) # ['network', 'database', 'security']
+print('-----------')
