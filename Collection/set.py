b6251ca7fb3b4c8418a9682c5eaa99d459f716fa
dai-gi <daisuke.u524@gmail.com>
2021-01-01 18:22:20 +0900

    修正

--- Collection/set.py
+++ Collection/set.py
@@ -1,4 +1,18 @@
 # セット
+"""""""""""""""""
+セットで使う関数・メソッド
+
+関数：
+・set関数
+・frozenset関数
+
+メソッド：
+・addメソッド
+・updateメソッド
+・removeメソッド
+・discardメソッド
+
+"""""""""""""""""
 test_set1 = {'python', '-', 'izm', '.', 'com'}
 print(test_set1) # {'izm', 'python', '-', 'com', '.'} <- 順番で出力されない
 print('-----------')
@@ -9,38 +23,3 @@ test_set3 = {1}
 print(type(test_set2)) # <class 'set'>
 print(type(test_set3)) # <class 'set'>
 print('-----------')
-
-
-# set関数
-set_func = set('python') # {'o', 'y', 'n', 'p', 't', 'h'}
-print(set_func)
-print('-----------')
-
-
-# addメソッド・updateメソッド(要素の追加)
-test_set2.add('pandas')
-print(test_set2) # {'python', 'pandas'}
-
-test_set3.update({2, 3})
-print(test_set3) # {1, 2, 3}
-print('-----------')
-
-
-# removeメソッド・discardメソッド(要素の削除)
-test_set1.remove('-')
-print(test_set1) # {'.', 'izm', 'com', 'python'}
-
-test_set1.discard('.')
-print(test_set1) # {'izm', 'com', 'python'}
-print('-----------')
-
-
-# frozenset
-test_set4 = frozenset({'python', '-', 'izm', '.', 'com'})
-print(type(test_set4)) # <class 'frozenset'>
-print(test_set4) # frozenset({'-', 'python', 'izm', '.', 'com'})
-
-# 定義すると要素を操作できなくなる
-test_set4.remove('-') # AttributeError: 'frozenset' object has no attribute 'remove'
-
-
