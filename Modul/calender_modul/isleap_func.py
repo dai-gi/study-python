64b87bdee05563adfbb1432bd16aa9063fcf6fff
dai-gi <daisuke.u524@gmail.com>
2021-01-01 23:12:26 +0900

    calender_modulフォルダに移動

--- Modul/calender_modul/isleap_func.py
+++ Modul/calender_modul/isleap_func.py
@@ -1,15 +1,3 @@
-# いちばんやさしいPython入門教室
-# calendarモジュール
-
-# from
-from calendar import month # <- 「from」を使うとモジュール名を書かずにメソッドを使うことが出来るようになる
-print(month(2020,1))
-
-# as
-import calendar as cal # <- 「as」を使うとモジュール名を変更する事ができる
-print(cal.month(2020,1))
-
-
 # Python-izm
 # isleapメソッド = その年がうるう年か判定する
 # 返り値は「boolean型」
