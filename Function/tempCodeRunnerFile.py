71be00b4891746f7da0c51a8070004a6799728db
dai-gi <daisuke.u524@gmail.com>
2021-01-03 11:05:12 +0900

    削除

--- Function/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-ntlist) # [0, 1, 2, 3, 4]
-# print(result) # <map object at 0x7f94dd4b9510>
-# print(list(result)) # [0, 1, 4, 9, 16]
-# #            ↑↑ リスト関数に「mapオブジェクト」の「result変数」を渡すと数値のリストに変換される
-
-# result = [x * x for x in intlist]
-# print(result) # [0, 1, 4, 9, 16]
