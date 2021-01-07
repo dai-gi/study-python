a6f8e37b002119bf81d2d79e381d9b841b7e8cb1
dai-gi <daisuke.u524@gmail.com>
2021-01-07 21:14:30 +0900

    first commit

---
+++ Modul/random_modul.py
@@ -0,0 +1,16 @@
+# @IT
+# import文：モジュールをインポートする
+import random
+
+print(random.randint(1, 20)) # 14
+
+# import as文：指定した名前でインポート
+import random as rnd
+
+print(rnd.randint(1, 20)) # 1
+print(random.randint(1, 20)) # 15
+
+# from import文：特定の関数だけをインポート
+from random import randint
+
+print(randint(1, 20)) # 4
