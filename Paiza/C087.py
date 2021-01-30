cfdbc68f9dedcc6b33d83a622eb87257c2735e20
dai-gi <daisuke.u524@gmail.com>
2021-01-30 15:51:45 +0900

    1. jugement関数を修正 2. while処理に変更
    1. 不要コメントの削除と返り値を変更するため
    2. 繰り返し処理する必要があるため。

--- Paiza/C087.py
+++ Paiza/C087.py
@@ -14,31 +14,27 @@ def reverse_and_calc(li):
     # print(reversed_li)
     
     result = n_1 + n_2
-    
     return result
     
-# it-swarm-ja.tech｜「Pythonを使用して、整数を反転し、回文を確認します」の質問に対する回答を参照
 def jugement(number):
-    a="" # 空の文字列を代入
-    x=str(number) # 文字列の整数を生成
-    # print(x)
-    for i in range(len(x),0,-1): # len(x) = x変数の桁数
-        # print(i)
-        a+=x[i-1] # 「x」の値を末尾から「a」に累積代入(複合代入)
-        # print(x[i-1])
-        # print(a)
-    if a==x: # a = 「x」の値を反転した文字列、これを「x」と比較し同じであれば、回文と判定
-        # print(x)
-        # print(a)
-        return True
+    n = ""
+    m = str(number)
+    for i in range(len(m),0,-1):
+        n += m[i-1]
+    if n == m:
+        return int(m)
     else:
-        print(x)
-        print(a)
-        return False
-
+        return list(m)
 
 # li_str = list(str(randint(1000, 9999)))
 li_str = list(input())
-num = reverse_and_calc(li_str)
 
-print(jugement(1222))
+isok = False
+while isok == False:
+    num = reverse_and_calc(li_str)
+    result = jugement(num)
+    if type(result) == int:
+        print(result)
+        isok = True
+    else:
+        li_str = result
